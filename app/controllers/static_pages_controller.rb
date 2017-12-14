14d9180bac9b872be13ca5d19a928dfde12c02e9
Tatsuki Okazaki <fnnyxe1339@gmail.com>
2017-12-14 12:28:55 +0000

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
+
   def home
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+      p "==============================================="
+      p @feed_items
+      p "==============================================="
+    end
   end
 
   def help
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
-  has_many :microposts, dependent: :destroy
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :microposts, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          validates :name,  presence: true, length: { maximum: 50 }
+         
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 end
